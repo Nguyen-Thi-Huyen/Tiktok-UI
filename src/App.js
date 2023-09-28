e8efbb83424ed5df6d2addcbe06c70ae7ba9ed0c
Nguyễn Thị Huyền <itnguyenhuyen@gmail.com>
2023-09-28 14:40:44 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {Route, Routes, Link} from 'react-router-dom'
+import HomePage from './pages/Home.js'
+import ContactPage from './pages/Contact.js'
+import NewsPage from './pages/News.js'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <ul>
+          <li><Link to="/">Homaaaaaaaaaae</Link></li>
+          <li><Link to="/news">News</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/news' element={<NewsPage/>}/>
+        <Route path='/contact' element={<ContactPage/>}/>
+      </Routes>
     </div>
   );
 }
